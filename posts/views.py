f4b0c76dbc4d617e09ace451b313ed67573e5882
Stanley <stan@Stanleys-MacBook-Pro.local>
2025-11-26 17:42:36 +0100

    Updated posts views

--- posts/views.py
+++ posts/views.py
@@ -11,7 +11,20 @@ from .models import Post
 # Create your views here.
 
 def home(request):
-    return HttpResponse('<h1>Welcome to our posts rest api service.</h1> <p>Visit the endpoint: <span style="color:green">/all/post/</span> to get started.</p>')
+    return HttpResponse('''div>
+                            <h1>Welcome to our posts rest api service.</h1> 
+                            <p>
+                                <strong>Visit these endpoints below to use our api:</strong> <br>
+                                <span style="color:green">/all/post/</span> - To view all posts <br>
+                                <span style="color:green">/create/post/</span> - To create a new post (Requires Authentication) <br>
+                                <span style="color:green">/update/post/<post_id>/</span> - To update a post (Requires Authentication) <br>
+                                <span style="color:green">/delete/post/<post_id>/</span> - To delete a post (Requires Authentication) <br>         
+                                <span style="color:green">/update/user/profile/</span> - To update user profile (Requires Authentication) <br>         
+                                <span style="color:green">/register/</span> - To register a new user <br>         
+                                <span style="color:green">/token/</span> - To obtain a token for authentication <br>         
+                                <span style="color:green">/token/refresh/</span> - To obtain a refresh token for authentication <br>         
+                            </p>
+                        </div>''')
 
 @api_view(['POST'])
 def register_user(request):
